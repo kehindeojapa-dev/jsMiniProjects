96f22538068aa40d60ac9a680f35df01c82102aa
Kehinde <adaeza.dev@gmail.com>
2020-10-27 19:05:37 +0000

    Tweak the homepage appearance(card content) and functionality(image showcase)

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ randomizeColors = function() {
 
 window.addEventListener('load', ()=> {
     setInterval(() => {
-        page.style.background=`linear-gradient(to right, ${randomizeColors()}, ${randomizeColors()})`;
-    }, 10000);
+        page.style.background=`linear-gradient(120deg, ${randomizeColors()}, ${randomizeColors()})`;
+    }, 15000);
 })
 
+
+// SELECTORS
+const images = document.querySelectorAll('.card img');
+const closeBtn = document.getElementById('close');
+const popup = document.querySelector('.popup');
+const popupImage = document.querySelector('.popup img');
+// console.log(popupImage.src);
+// EVENT LISTENERS
+images.forEach(function(image) {
+    image.addEventListener('click', (e)=> {
+        popupImage.src = e.target.src;
+        popup.classList.add('active');
+        document.body.addEventListener('keydown', (e)=> {
+            if(event.key == 'Escape') {
+                popup.classList.remove('active');
+            }
+        })
+    })
+})
+
+closeBtn.addEventListener('click', ()=> {
+    popup.classList.remove('active');
+});
